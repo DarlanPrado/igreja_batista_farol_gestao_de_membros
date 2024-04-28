95d651b197f551f8cc5e2b913b20a270c64ed664
DarlanPrado <darlandoprado2014@gmail.com>
2024-04-28 17:55:06 -0300

    :bug: removido declaração de plugin virtual

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  nitro: {
-    plugins: ['~/server/models/Usuarios']
-  },
   routeRules: {
     '/': {
       redirect: {
