901d0fe3bf2ec32be9ffaeb5e82303a699a2c52c
DarlanPrado <darlandoprado2014@gmail.com>
2024-05-02 23:55:15 -0300

    :bug: corrrigido responsividade da tela de login

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,25 +1,30 @@
 <template>
-    <div class="bg-gray-200 dark:bg-gray-900 p-6 rounded w-96">
-        <UForm :schema="schemaLogin" :state="stateLogin" @submit="logar" class="space-y-2">
-            <UFormGroup size="md" label="Email" name="email">
-                <UInput v-model="stateLogin.email"/>
-            </UFormGroup>
-            <UFormGroup size="md" label="Senha" name="senha">
-                <!-- <UInput v-model="stateLogin.senha" /> -->
-                <UButtonGroup class="w-full" size="md" orientation="horizontal">
-                    <UInput v-model="stateLogin.senha" :type=" showPassword ? 'text':'password'" class="w-full" />
-                    <UTooltip :text="showPassword ? 'Ocultar senha' : 'Mostrar senha'">
-                        <UButton @click="showPassword = !showPassword" :icon="showPassword ? 'i-heroicons-eye-slash-solid' : 'i-heroicons-eye-solid'" color="gray" />
-                    </UTooltip>
-                </UButtonGroup>
-            </UFormGroup>
-            <div class="flex justify-end">
-                <NuxtLink :to="`/login/esqueciASenha/${stateLogin.email ? stateLogin.email : ''}`" class="hover:text-primar cursor-pointer" text="Esqueci a senha" />
-            </div>  
-            <div class="flex justify-center">
-                <UButton class="text-gray-900" size="md" type="submit" label="Entrar" :variant="useColorMode().value == 'dark' ? 'outline' : 'solid'" block/>
-            </div>
-        </UForm>
+    <div class="h-full w-full flex justify-center items-center">
+        <!-- <div class="bg-gray-200 dark:bg-gray-900 p-6 rounded w-96">
+            
+        </div> -->
+        <UCard :ui="{ base: 'w-full sm:w-[60%] md:w-[40%]' }">
+            <UForm :schema="schemaLogin" :state="stateLogin" @submit="logar" class="space-y-2">
+                <UFormGroup size="md" label="Email" name="email">
+                    <UInput v-model="stateLogin.email"/>
+                </UFormGroup>
+                <UFormGroup size="md" label="Senha" name="senha">
+                    <!-- <UInput v-model="stateLogin.senha" /> -->
+                    <UButtonGroup class="w-full" size="md" orientation="horizontal">
+                        <UInput v-model="stateLogin.senha" :type=" showPassword ? 'text':'password'" class="w-full" />
+                        <UTooltip :text="showPassword ? 'Ocultar senha' : 'Mostrar senha'">
+                            <UButton @click="showPassword = !showPassword" :icon="showPassword ? 'i-heroicons-eye-slash-solid' : 'i-heroicons-eye-solid'" color="gray" />
+                        </UTooltip>
+                    </UButtonGroup>
+                </UFormGroup>
+                <div class="flex justify-end">
+                    <NuxtLink :to="`/login/esqueciASenha/${stateLogin.email ? stateLogin.email : ''}`" class="hover:text-primar cursor-pointer" text="Esqueci a senha" />
+                </div>  
+                <div class="flex justify-center">
+                    <UButton class="text-gray-900" size="md" type="submit" label="Entrar" :variant="useColorMode().value == 'dark' ? 'outline' : 'solid'" block/>
+                </div>
+            </UForm>
+        </UCard>
     </div>
 </template>
 <script setup lang="ts">
